d1b85f052f2195939c89b1f75b7391947cdac324
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2021-11-22 08:50:00 -0800

    I wrote comments that outlined my different methods

--- BirthdayProject/src/Birthday.java
+++ BirthdayProject/src/Birthday.java
@@ -1,13 +1,30 @@
 /*
- * Name
- * Date
- * APCS Period
+ * Isayiah Lim
+ * 11/22/2021
+ * APCS Period 2
  * Birthday Project
  * 
  * Program Description
+ * This program asks for today's date and the user's birthday, then calculates the amount of days
+ * before their next birthday. It also prints out the absolute date of both.
  */
 
 public class Birthday {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+    	
     }
+    
+    //gives the range for days based off of the month inputed
+    
+
+    //calculates the absolute days of any date
+
+    
+    //calculates the days between the birthday and the current day
+    
+    
+    //prints out the final statement based off days between the two dates
+    
+    
 }
