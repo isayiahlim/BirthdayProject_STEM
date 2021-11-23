926c5ff8ef0281f77dd694d1213a05c17dbd5b7d
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2021-11-23 08:53:28 -0800

    This code should work as intended. I'm working on reducing redundancy.

--- BirthdayProject/src/Birthday.java
+++ BirthdayProject/src/Birthday.java
@@ -18,26 +18,38 @@ public class Birthday
     	System.out.println("it will be until your next birthday.");
     	System.out.println();
     	
-    	//prints the first statement
+    	//gets today's date and prints out the absolute date
     	System.out.println("Please enter today's date:");
-    	monthAndDay(input);
+    	int todaysAbsDate = monthAndDay(input);
+    	
+    	//prints a new line
+    	System.out.println();
+    	
+    	//gets the birthday and prints out the absolute date
+    	System.out.println("Please enter your birthday:");
+    	int bDayAbsDate = monthAndDay(input);
+    	
+    	//prints the final statement
+    	finalStatement(todaysAbsDate,bDayAbsDate);
     }
     
     //first statement
-    public static void monthAndDay(Scanner input)
+    public static int monthAndDay(Scanner input)
     {
 
-    	System.out.println("What is the month (1-12)?"); 
+    	System.out.print("What is the month (1-12)? "); 
     	int month = input.nextInt();
     	
     	//calls the length function to find the length of the month
     	int monthLength = dateRange(month);
-    	System.out.println("What is the day (1-" + monthLength + ")?");
+    	System.out.print("What is the day (1-" + monthLength + ")? ");
     	int day = input.nextInt();
     	
     	//calls the absDate function to find the absolute distance between the days
     	int absNum = absDate(month, day);
     	System.out.println(month + "/" + day + " is the day #" + absNum + " of 365.");
+    	
+    	return absNum;
     }
     
     //returns the number of days based off of the month inputed
@@ -57,7 +69,6 @@ public class Birthday
     //calculates the absolute days of any date
     public static int absDate(int month, int day)
     {
-    	int number = 0;
     	//adds the number of days before the month to the day inputed
     	if(month == 12)
     		return 334 + day;
@@ -65,19 +76,40 @@ public class Birthday
     		return 304 + day;
     	else if (month == 10)
     		return 273 + day;
-    	else if ()
+    	else if (month == 9)
+    		return 243 + day;
+    	else if (month == 8)
+    		return 212 + day;
+    	else if (month == 7)
+    		return 181 + day;
+    	else if (month == 6)
+    		return 151 + day;
+    	else if (month == 5)
+    		return 120 + day;
+    	else if (month == 4)
+    		return 90 + day;
+    	else if (month == 3)
+    		return 59 + day;
+    	else if (month == 2)
+    		return 31 + day;
+    	else
+    		return day;
     }
-    
-    //calculates the days between the birthday and the current day
-    public static int distance(int today, int bday)
-    {
-    	
-    }
-    
+        
     //prints out the final statement based off days between the two dates
-    public static void finalStatement() 
+    public static void finalStatement(int today, int bday) 
     {
-    	
+    	int distance = bday - today;
+    	if(distance < 0)
+    		System.out.println("Your birthday is in " + (365 - today + bday) + " days.");
+    	//special cases
+    	else if(distance == 1)
+    		System.out.println("Wow, your birthday is tomorrow!");
+    	else if(distance == 0)
+    		System.out.println("Happy Birthday!");
+    	else
+    		System.out.println("Your next birthday is in " + distance + " days."));
     }
-    
+
 }
+
